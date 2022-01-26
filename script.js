ee64438c9c9e442fa891af5bf0169dd133ceb063
Or Galili <OrG@Taldor.corp>
2022-01-26 15:35:18 +0200

    copy source file after change to array object of lines

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ rulesFile.split(/\r?\n/).forEach(line =>  {
   let rule = line.split("-");
   ruleArr.push({source:rule[0] , dest:rule[1]});
 });
+
+let dest;
+let sourceFile = fs.readFileSync('source.txt','utf-8');
+dest = sourceFile.split(/\r?\n/).map(line =>  {
+	ruleArr.forEach(rule=>{
+		if(line.includes(rule.source)){
+			line = line.replaceAll(rule.source,rule.dest);
+		}
+	})
+	return line;
+});
