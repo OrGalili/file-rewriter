55253f87706ad031d781fe0304e7c285a8791af5
orgalili <orgalil21@gmail.com>
2022-02-25 00:27:23 +0200

    documentation, no need require util package

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 const fs = require('fs');
-const util = require('util');
 //npm i iconv-lite
 const iconv = require('iconv-lite');
-const ruleArr = [];
 
-//---- streaming rules file to string array object.
+//---- 1.streaming rules file to string array object.
+const ruleArr = [];
 const rulesFile = fs.readFileSync('rules.txt', 'utf-8');
 rulesFile.split(/\r?\n/).forEach(line =>  {
   let rule = line.split("-");
   ruleArr.push({source:rule[0] , dest:rule[1]});
 });
 
-//---- Reading the source file and writing to a new file according to the rules.
+//---- 2.Reading the source file and writing to a new file while preserving the encoding.
 let sourceBuffer = fs.readFileSync('source.txt');// streaming the file to a buffer
 const decoder = new TextDecoder('windows-1255'); // creating decoder with a selected encoding (in this case - windows1255).
 let sourceStr = decoder.decode(sourceBuffer, { stream: true }); // decoding the buffer to a string.
 sourceStr += decoder.decode(); // end-of-stream. result - a decoded string with source file content.
+
+//---- 3.Replacing subtrings according to the rules.
 ruleArr.forEach(rule=>{
 	sourceStr = sourceStr.replaceAll(rule.source,rule.dest);//replaces all sub-strings found in the rules file.
 })
 
-fs.writeFileSync('intermediate.txt',sourceStr);
+//---- 4.Writing the change string to a new file while preserving the encoding.
+fs.writeFileSync('intermediate.txt',sourceStr);// in nodejs the file that created is in UTF-8 encode.
 
-fs.createReadStream('intermediate.txt')
-    .pipe(iconv.decodeStream('utf8'))
-    .pipe(iconv.encodeStream('win1255'))
-    .pipe(fs.createWriteStream('dest.txt'));
+fs.createReadStream('intermediate.txt') // Writing the intermediate file
+    .pipe(iconv.decodeStream('utf8')) //from UTF-8 encode
+    .pipe(iconv.encodeStream('win1255')) // to windows-1255 encode (in this case)
+    .pipe(fs.createWriteStream('dest.txt')); // to a new file called dest.
 
 
 //---- creating a log file- where the changes has been made and what rule has been applied for each line.
-let log = "line |  rule\n";
+let log = "line          |  rule\n";
 let sourceFile = fs.readFileSync('source.txt','utf-8');
 sourceFile.split(/\r?\n/).map((line,index) =>  {
 	ruleArr.forEach(rule=>{
