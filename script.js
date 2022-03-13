953e80fbd4f1686054829d95d06b726e95717477
Or Galili <orgalil21@gmail.com>
2022-03-13 18:30:22 +0200

    change doc comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 //npm i iconv-lite
 const iconv = require('iconv-lite');
 
-//---- 1.streaming rules file to string array object.
+//---- streaming rules file to string array object.
 const ruleArr = [];
 const rulesFile = fs.readFileSync('rules.txt', 'utf-8');
 rulesFile.split(/\r?\n/).forEach(line =>  {
@@ -10,21 +10,22 @@ rulesFile.split(/\r?\n/).forEach(line =>  {
   ruleArr.push({source:rule[0] , dest:rule[1]});
 });
 
-//---- 2.Reading the source file and writing to a new file while preserving the encoding.
+//---- 1.Reading the source file and writing to a string while preserving the encoding.
 let sourceBuffer = fs.readFileSync('source.txt');// streaming the file to a buffer
 const decoder = new TextDecoder('windows-1255'); // creating decoder with a selected encoding (in this case - windows1255).
 let sourceStr = decoder.decode(sourceBuffer, { stream: true }); // decoding the buffer to a string.
 sourceStr += decoder.decode(); // end-of-stream. result - a decoded string with source file content.
 
-//---- 3.Replacing subtrings according to the rules.
+//---- 2.Replacing subtrings according to the rules.
 ruleArr.forEach(rule=>{
 	sourceStr = sourceStr.replaceAll(rule.source,rule.dest);//replaces all sub-strings found in the rules file.
 })
 
-//---- 4.Writing the change string to a new file while preserving the encoding.
+//---- 3.Writing the change string to a new file while preserving the encoding.
 fs.writeFileSync('intermediate.txt',sourceStr);// in nodejs the file that created is in UTF-8 encode.
 
-fs.createReadStream('intermediate.txt') // Writing the intermediate file
+//---- 4. writing from the intermidiate file to a new file in order to preserve the encoding
+fs.createReadStream('intermediate.txt') // access to the intermediate file
     .pipe(iconv.decodeStream('utf8')) //from UTF-8 encode
     .pipe(iconv.encodeStream('win1255')) // to windows-1255 encode (in this case)
     .pipe(fs.createWriteStream('dest.txt')); // to a new file called dest.
